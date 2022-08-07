9512d7ce0b0efdfbc711d4fdef413fb327d5f4eb
Yash <yashatkulwar2000@gmail.com>
2022-08-07 17:01:16 +0530

    added single player at starting position

---
+++ LadderSnake.java
@@ -0,0 +1,23 @@
+package laddersnake;
+
+import java.util.Random;
+
+class gamePlayer {
+
+    int currentPosition = 0;
+
+    Random rand = new Random();
+
+    int attempts = 0;
+
+    final int noPlay = 0, ladder = 1, snake = 2;
+
+    int endGame = 0;
+
+}
+public class LadderSnake {
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder games");
+
+    }
+}
