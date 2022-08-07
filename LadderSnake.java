90e8f8577e65fc8dd41ce23a72b2a26e15548de8
Yash <yashatkulwar2000@gmail.com>
2022-08-07 17:20:46 +0530

    player reaches 100 to win

--- LadderSnake.java
+++ LadderSnake.java
@@ -10,10 +10,68 @@ class gamePlayer {
 
     final int noPlay = 0, ladder = 1, snake = 2;
 
-    int dieRoll = rand.nextInt(6)+1;
+    int endGame = 0;
 
-    int gameCheck = rand.nextInt(3);
+    int attempts = 0;
 
+    public void gameStart(String playerName) {
+
+        while (endGame <= 100) {
+
+            int rollDie = rand.nextInt(6) + 1;
+
+            System.out.println("rollDie is :" + rollDie);
+
+            int gameCheck = rand.nextInt(3);
+
+            System.out.println("game Check is " + gameCheck);
+
+            if (gameCheck == noPlay) {
+
+                currentPosition = currentPosition;
+
+                System.out.println("This is no play.");
+
+                System.out.println(playerName + "'s current position is : " + currentPosition);
+
+                attempts++;
+
+            } else if (gameCheck == ladder) {
+
+                currentPosition = currentPosition + rollDie;
+
+                System.out.println("You got promoted by " + rollDie);
+
+                System.out.println(playerName + "'s current position is : " + currentPosition);
+
+                attempts++;
+
+            } else {
+
+                if (currentPosition < rollDie) {
+
+                    currentPosition = 0;
+
+                    System.out.println("You got snake of " + rollDie + " star again from zero.");
+
+                    System.out.println(playerName + "'s current position is : " + currentPosition);
+
+                } else {
+
+                    currentPosition = currentPosition - rollDie;
+
+                    System.out.println("you got snake of " + rollDie);
+
+                    System.out.println(playerName + "'s current position is : " + currentPosition);
+                }
+
+                attempts++;
+
+            }
+            endGame = currentPosition;
+        }
+
+    }
 }
 public class LadderSnake {
     public static void main(String[] args) {
